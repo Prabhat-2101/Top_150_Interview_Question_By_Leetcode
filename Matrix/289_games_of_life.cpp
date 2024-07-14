daf91fe4a6dd504ba2af0b79684014d7a6c00852
Prabhat-2101 <prabhatkumarraj2003@gmail.com>
2024-07-15 01:08:43 +0530

    15-07-2024

---
+++ Matrix/289_games_of_life.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int helper(vector<vector<int>> board,int i,int j,int rows,int cols){
+        int count = 0;
+        for (int k = i - 1; k <= i + 1; k++) {
+            for (int h = j - 1; h <= j + 1; h++) {
+                if (k >= 0 && k < rows && h >= 0 && h < cols && !(k == i && h == j)) {
+                    if (board[k][h] == 1 || board[k][h] == 2) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+    void gameOfLife(vector<vector<int>>& board) {
+        int rows = board.size();
+        int cols = board[0].size();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                int count = helper(board,i,j,rows,cols);
+                if (board[i][j] == 0 && count == 3) {
+                    board[i][j] = -1;
+                } else if ((count < 2 || count > 3) && board[i][j] == 1) {
+                    board[i][j] = 2;
+                }
+            }
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (board[i][j] == -1) {
+                    board[i][j] = 1;
+                } else if (board[i][j] == 2) {
+                    board[i][j] = 0;
+                }
+            }
+        }
+    }
+};
